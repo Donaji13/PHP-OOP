ca5d2b1584dc5760bb96399c88845ca5b6287c22
donaji13 <toltekochoa12@gmail.com>
2015-01-12 13:23:32 -0800

    class work php update

---
+++ update.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class updatephp 
+{
+	public $new;
+	public $php;
+	public $code;
+function __construct($new, $php, $code) {
+	$this->new = $new;
+	$this->php = $php;
+	$this->code = $code;
+}
+function getNew() {
+	return $this­>new; .
+	$this­>php;
+	}
+}

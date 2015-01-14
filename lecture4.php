edfeb85c200cb3f76135db162efcd201058030ee
donaji13 <toltekochoa12@gmail.com>
2015-01-14 11:44:24 -0800

    lecture 4

---
+++ lecture4.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+class Fifa {
+	public $Points
+	public $Coins
+	public $Messi
+	public $Packs
+	
+	function __construct($Points, $Coins, $Messi, $Packs)
+	{
+		
+	$this->Messi=$Messi;
+	$this->Points=$Points;
+	$this->Coins=$Coins;
+	$this->Packs=$Packs;
+
+	}
+}
